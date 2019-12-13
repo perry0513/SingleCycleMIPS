b3c3b1ae396ce9e58e36576dc3e325f56b8990b2
b05901172 <b05901172>
2019-12-13 23:08:49 +0800

    add slt

--- alu.v
+++ alu.v
@@ -18,6 +18,7 @@ output [31:0] ALUResult;
 assign ALUResult = ALUCtrl[2:1] == 2'b00 ? ( ALUCtrl[0] == 1'b0 ? in0 & in1 : in0 | in1 ) :
                    ALUCtrl[2:1] == 2'b10 ? ( ALUCtrl[0] == 1'b0 ? in1 << shamt : in1 >> shamt ) :
                    ALUCtrl[1:0] == 2'b10 ? ( ALUCtrl[2] == 1'b0 ? in0 + in1 : in0 - in1 ) :
+                   ALUCtrl[1:0] == 2'b11 ? { 31'b0, (in0 < in1) } :
                    32'b0;
 
 endmodule
